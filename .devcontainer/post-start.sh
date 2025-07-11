038978557e4541b1b23006227de50a37cc00d026
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-07-11 16:27:07 +0000

    Merge branch 'framework/main'. Adding Mulitarchitecture, supporting ARM64/AMD64 and image pull. Also running locally in docker container and vs code

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,6 +8,6 @@ source /workspaces/$RepositoryName/.devcontainer/util/functions.sh
 
 #TODO: BeforeGoLive comment this so the Mkdocs are not exposed in the container.
 # we want to monitor all interactions of the users in the live github pages.
-exposeMkdocs
+#exposeMkdocs
 
-printInfoSection "Your dev.container finished creating"
+printInfoSection "Your dev.container finished starting up"
