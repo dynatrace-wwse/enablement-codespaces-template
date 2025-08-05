73eae321c47dfaf9d02bce48eba8769fdcc94afd
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-08-05 16:25:27 +0100

    remove test DT components
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -3,12 +3,6 @@ source /workspaces/$RepositoryName/.devcontainer/test/test_functions.sh
 
 printInfoSection "Running integration Tests for the Enablement Framework"
 
-assertRunningPod dynatrace operator
-
-assertRunningPod dynatrace activegate
-
-assertRunningPod dynatrace oneagent
-
 assertRunningPod todoapp todoapp
 
 assertRunningApp 30100
