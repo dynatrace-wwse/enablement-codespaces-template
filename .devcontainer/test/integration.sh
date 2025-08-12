3111c6c03fe1d69a98bf9a0dcef2aaf7380fbd6f
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-08-12 14:13:50 +0100

    Rename integration tests and add power10k

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -7,6 +7,12 @@ source $REPO_PATH/.devcontainer/test/test_functions.sh
 
 printInfoSection "Running integration Tests for the Enablement Framework"
 
+assertRunningPod dynatrace operator
+
+assertRunningPod dynatrace activegate
+
+assertRunningPod dynatrace oneagent
+
 assertRunningPod todoapp todoapp
 
 assertRunningApp 30100
